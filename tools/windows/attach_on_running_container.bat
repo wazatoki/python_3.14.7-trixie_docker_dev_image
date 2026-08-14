f07c3dbcd3699b757a439dd17593a556e69903c4
wazatoki <wazatoki@gmail.com>
2026-08-14 16:43:14 +0900

    change work dir

--- tools/windows/attach_on_running_container.bat
+++ tools/windows/attach_on_running_container.bat
@@ -1,4 +1,4 @@
 rem attach on running container
 
 cd /d %~dp0
-docker compose -f ..\docker\docker-compose.yml --env-file ..\..\.env exec --user dev python_3.14.7_ssh_container /bin/bash
+docker compose -f ..\docker\docker-compose.yml --env-file ..\..\.env exec --user dev python_3.14.7_ssh_div_container /bin/bash
